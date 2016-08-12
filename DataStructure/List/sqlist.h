60e96d3d91e284131876540f8aebf9772e2d855c
lightningkay <ew_34@sina.com>
2016-08-12 23:11:21 +0800

    data

---
+++ DataStructure/List/sqlist.h
@@ -0,0 +1,19 @@
+/*************************************************************************
+ > File Name: sqlist.h
+ > Author: 
+ > Mail: 
+ > Created Time: 2016年08月12日 星期五 23时02分08秒
+ ************************************************************************/
+
+#ifndef _SQLIST_H
+#define _SQLIST_H
+
+#define maxSize 100
+
+typedef struct
+{
+    int data[maxSize];
+
+    int length;
+} Sqlist;
+#endif
