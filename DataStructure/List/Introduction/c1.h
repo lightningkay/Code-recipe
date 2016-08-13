e7fc4478e280eb7fa2d9815f5c25c9e8134c7370
lightningkay <ew_34@sina.com>
2016-08-13 22:57:18 +0800

    introduction

---
+++ DataStructure/List/Introduction/c1.h
@@ -0,0 +1,16 @@
+#include <string.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream.h>
+#include <math.h>
+#include <process.h>
+#include <limits.h>
+
+#define TRUE 1
+#define FALSE 0
+#define OK 1
+#define ERROR 0
+#define INFEASIBLE -1
+typedef int Status;
+typedef int Boolean;
